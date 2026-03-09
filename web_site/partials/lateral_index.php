7933679ca780ebc2d203100b4e8ac33d5af918b1
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-03-09 12:08:21 -0600

    fix: Corregir imágenes y enlaces en el panel lateral (insumos y clientes)

--- web_site/partials/lateral_index.php
+++ web_site/partials/lateral_index.php
@@ -1,5 +1,4 @@
 
-
         <?php
 require_once __DIR__ . '/../../classbox/config/database.php';
 
@@ -25,9 +24,9 @@ try {
               <?php if (!empty($recent_posts)): ?>
                   <?php foreach ($recent_posts as $post): ?>
                   <div class="post-item">
-                    <img src="<?php echo BASE_URL; ?>/<?php echo htmlspecialchars($post['main_image']); ?>" alt="" class="flex-shrink-0" style="box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);">
+                    <img src="<?php echo BASE_URL; ?>/public/uploads/images/<?php echo htmlspecialchars($post['main_image']); ?>" alt="" class="flex-shrink-0" style="box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);">
                     <div>
-                      <h4><a href="/web_site/blog_details.php?id=<?php echo htmlspecialchars($post['id_post']); ?>"><?php echo htmlspecialchars($post['title']); ?></a></h4>
+                      <h4><a href="/web_petrodrill/web_site/blog_details.php?id=<?php echo htmlspecialchars($post['id_post']); ?>"><?php echo htmlspecialchars($post['title']); ?></a></h4>
                       <time datetime="<?php echo htmlspecialchars($post['created_at']); ?>"><?php echo date('M d, Y', strtotime($post['created_at'])); ?></time>
                     </div>
                   </div><!-- End recent post item-->
@@ -57,9 +56,9 @@ try {
               <?php if (!empty($clientes_posts)): ?>
                   <?php foreach ($clientes_posts as $post): ?>
                   <div class="post-item">
-                    <img src="<?php echo BASE_URL; ?>/<?php echo htmlspecialchars($post['main_image']); ?>" alt="" class="flex-shrink-0" style="box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);">
+                    <img src="<?php echo BASE_URL; ?>/public/uploads/images/<?php echo htmlspecialchars($post['main_image']); ?>" alt="" class="flex-shrink-0" style="box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);">
                     <div>
-                      <h4><a href="/web_site/blog_details.php?id=<?php echo htmlspecialchars($post['id_post']); ?>"><?php echo htmlspecialchars($post['title']); ?></a></h4>
+                      <h4><a href="/web_petrodrill/web_site/blog_details.php?id=<?php echo htmlspecialchars($post['id_post']); ?>"><?php echo htmlspecialchars($post['title']); ?></a></h4>
                       <time datetime="<?php echo htmlspecialchars($post['created_at']); ?>"><?php echo date('M d, Y', strtotime($post['created_at'])); ?></time>
                     </div>
                   </div><!-- End recent post item-->
@@ -70,12 +69,6 @@ try {
 
             </div><!--/Recent Posts Widget -->
 
-            
-
-            
-
           </div>
 
         </div>
-
-      
