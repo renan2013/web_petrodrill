564fd6e76ff396c3ddb8ef18329406a32da27e95
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-03-10 13:20:57 -0600

    fix: Corregir rutas de assets y favicon en head_section.php

---
+++ index_viejo/index.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include 'web_site/partials/head_section.php';
+include 'web_site/partials/header_nav.php';
+include 'web_site/partials/slider.php';
+include 'web_site/partials/main_start.php';
+
+echo '<!-- DEBUG: Antes de cuerpo_index.php -->';
+include 'web_site/partials/cuerpo_index.php';
+echo '<!-- DEBUG: Después de cuerpo_index.php -->';
+
+include 'web_site/partials/main_end.php';
+echo '<!-- DEBUG: Antes de footer_scripts.php -->';
+include 'web_site/partials/footer_scripts.php';
+echo '<!-- DEBUG: Después de footer_scripts.php -->';
+?>
--- web_site/partials/head_section.php
+++ web_site/partials/head_section.php
@@ -9,8 +9,8 @@
   <meta name="keywords" content="repuestos de perforacion, metal mecanica, importacion y exportacion de insumos industriales">
 
   <!-- Favicons -->
-  <link href="/web_site/assets/img/favicon.png" rel="icon">
-  <link href="/web_site/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="/web_petrodrill/web_site/assets/img/favicon.png" rel="icon">
+  <link href="/web_petrodrill/web_site/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
@@ -18,22 +18,15 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="/web_site/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/web_site/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="/web_site/assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="/web_site/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link href="/web_site/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="/web_petrodrill/web_site/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/web_petrodrill/web_site/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="/web_petrodrill/web_site/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="/web_petrodrill/web_site/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="/web_petrodrill/web_site/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
 
   <!-- Main CSS File -->
-  <link href="/web_site/assets/css/main.css?v=<?php echo time(); ?>" rel="stylesheet">
+  <link href="/web_petrodrill/web_site/assets/css/main.css?v=<?php echo time(); ?>" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: Blogy
-  * Template URL: https://bootstrapmade.com/blogy-bootstrap-blog-template/
-  * Updated: Feb 22 2025 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body class="index-page">
